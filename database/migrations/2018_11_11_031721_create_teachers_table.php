f8a22a94214530817990e5499f247d3d19d40c44
ing <inyoman_gurnitha@yahoo.com>
2018-11-11 10:20:45 +0700

    04.2 Profesores, estudiantes, categorías, niveles y cursos

---
+++ database/migrations/2018_11_11_031721_create_teachers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('title')->nullable();
+            $table->text('biography')->nullable();
+            $table->string('website_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
---
+++ database/migrations/2018_11_11_031811_create_courses_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('teacher_id');
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->unsignedInteger('level_id');
+            $table->foreign('level_id')->references('id')->on('levels');
+            $table->string('name');
+            $table->text('description');
+            $table->string('slug');
+            $table->enum('status', [
+                \App\Course::PUBLISHED,
+                \App\Course::PENDING,
+                \APP\Course::REJECTED
+            ])->default(\App\Course::PENDING);
+            $table->boolean('previous_approached')->default(false);
+            $table->boolean('previous_rejected')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
